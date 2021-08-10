ae355a396d5119f693a3c521c5fc2fa8b101c48d
Saurab Prasher <saurab015@gmail.com>
2021-08-10 20:57:19 +0530

    code showing in iframe

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,12 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const iframe = useRef<any>();
   const ref = useRef<any>();
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -35,8 +36,33 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      
+
+    <div id="root">
+    
+    </div>
+
+    <script>
+    window.addEventListener('message',(event)=>{
+      eval(event.data)
+    },false)</script>
+    
+    </body>
+    </html>
+  `;
   return (
     <div>
       <textarea
@@ -46,6 +72,8 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='test' />
+
       <pre>{code}</pre>
     </div>
   );
