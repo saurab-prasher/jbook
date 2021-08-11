18ffee39aff07d47d4a9b128ec860ec37ae5a90a
Saurab Prasher <saurab015@gmail.com>
2021-08-11 14:36:38 +0530

    monaco  editor added

---
+++ src/plugins/code-editor.tsx
@@ -0,0 +1,16 @@
+import MonacoEditor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+  return (
+    <MonacoEditor
+      language='javascript'
+      theme='dark'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
